c4fab46bb14f8071381f93861bd2cbc781b7b75f
fvanroie <cpt_jack@msn.com>
2020-12-04 03:52:00 +0100

    Update BuildPath for actions

--- MqttClientPlugin/AssemblyInfo.cs
+++ MqttClientPlugin/AssemblyInfo.cs
@@ -3,12 +3,12 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCopyright("©2020 - NetwiZe.be")]
-[assembly: AssemblyVersion("0.2.0.1001")]
-[assembly: AssemblyFileVersion("0.2.0.1001")]
+[assembly: AssemblyVersion("0.2.0.1204")]
+[assembly: AssemblyFileVersion("0.2.0.1204")]
 #if X64
-[assembly: AssemblyInformationalVersion("0.2.0.1001 (64-bit)")]
+[assembly: AssemblyInformationalVersion("0.2.0.1204 (64-bit)")]
 #else
-[assembly: AssemblyInformationalVersion("0.2.0.1001 (32-bit)")]
+[assembly: AssemblyInformationalVersion("0.2.0.1204 (32-bit)")]
 #endif
 [assembly: AssemblyProduct("MqqtPlugin for Rainmeter")]
 [assembly: AssemblyTitle("MqqtPlugin")]
